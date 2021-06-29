18566dfecdd23efc8934f3fa0808f9c1dcd726cb
Michal Rogowski <michrogowski@gmail.com>
2021-06-29 23:50:16 +0200

    Add sample API

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+app = FastAPI()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+@app.get("/questions")
+async def questions():
+    return [
+        {
+        "name":"test",
+        "id":"123"
+        }
+    ]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.post("/questions/{question_id}")
+async def post_answer(question_id: str):
+    return {"question_id": question_id}
